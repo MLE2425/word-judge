a095f70d925732ad0ce23bad6598c6e5952ae99a
adrrf <adrf.dev@gmail.com>
2025-01-09 19:17:09 +0100

    fix: nltk downloads

--- src/data/nlp.py
+++ src/data/nlp.py
@@ -1,42 +1,45 @@
 import logging
-from transformers import AutoTokenizer
-import nltk
 import string
+
+import nltk
+from transformers import AutoTokenizer
 
 logger = logging.getLogger(__name__)
 
-tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 
-try:
-    stopwords: set[str] = set(nltk.corpus.stopwords.words("english"))
-except LookupError:
-    nltk.download("stopwords")
-    stopwords: set[str] = set(nltk.corpus.stopwords.words("english"))
+def tokenize(
+    text: str, remove_stopwords: bool, lemmatize: bool
+) -> list[str] | list[list[str]]:
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 
-punctuation: set[str] = set(string.punctuation)
-punctuation.add("’")
+    punctuation: set[str] = set(string.punctuation)
+    punctuation.add("’")
 
-try:
-    lemmatizer = nltk.stem.WordNetLemmatizer()
-except OSError:
-    nltk.download("wordnet")
-    lemmatizer = nltk.stem.WordNetLemmatizer()
-
-
-def tokenize(text: str, remove_stopwords: bool, lemmatize: bool) -> list[str]:
     tokens: list[str] = tokenizer.tokenize(text, add_special_tokens=False)
 
     if remove_stopwords:
-        tokens = [token for token in tokens if token not in stopwords]
+        try:
+            stopwords: set[str] = set(nltk.corpus.stopwords.words("english"))
+            tokens = [token for token in tokens if token not in stopwords]
+        except LookupError:
+            nltk.download("stopwords")
+            stopwords = set(nltk.corpus.stopwords.words("english"))
+            tokens = [token for token in tokens if token not in stopwords]
 
     if lemmatize:
-        tokens = [lemmatizer.lemmatize(token) for token in tokens]
+        try:
+            lemmatizer = nltk.stem.WordNetLemmatizer()
+            tokens = [lemmatizer.lemmatize(token) for token in tokens]
+        except LookupError:
+            nltk.download("wordnet")
+            lemmatizer = nltk.stem.WordNetLemmatizer()
+            tokens = [lemmatizer.lemmatize(token) for token in tokens]
 
     tokens = [token for token in tokens if token not in punctuation]
 
     chunk_size: int = 510  # 512 - 2
 
-    chunks: list[str] = [
+    chunks: list[list[str]] = [
         tokens[i : i + chunk_size] for i in range(0, len(tokens), chunk_size)
     ]
 
@@ -47,7 +50,9 @@ def tokenize(text: str, remove_stopwords: bool, lemmatize: bool) -> list[str]:
     return chunks
 
 
-def encode(chunks: list[list[str]]) -> list[int]:
+def encode(chunks: list[list[str]]) -> list[int] | list[list[int]]:
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+
     encoded_chunks: list[list[int]] = [
         tokenizer.convert_tokens_to_ids(chunk) for chunk in chunks
     ]
